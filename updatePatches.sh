cdac660c25357c8c61fde304a0786e9bea510f47
timoreo22 <timo.oreo34@gmail.com>
2021-07-01 20:55:59 +0200

    Create updatePatches.sh

---
+++ updatePatches.sh
@@ -0,0 +1,9 @@
+# delete patches
+rm patches/server/0123-Properly-fix-item-duplication-bug.patch
+rm patches/server/0345-Fix-MC-161754.patch
+rm patches/server/0424-Fix-numerous-item-duplication-issues-and-teleport-is.patch
+rm patches/server/0556-Expose-world-spawn-angle.patch
+rm patches/server/0578-Fix-harming-potion-dupe.patch
+rm patches/server/0715-Fix-PlayerDropItemEvent-using-wrong-item.patch
+
+#TODO fix other patches ?
